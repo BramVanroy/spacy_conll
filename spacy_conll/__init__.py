fbbb56d7f7e1311f94e3bc58893bbec17676d32d
BramVanroy <Bram.Vanroy@UGent.be>
2020-01-15 13:26:24 +0100

    Reworked extensions so that a str, a str including headers, and conll in tuples are added to custom extensions

--- spacy_conll/__init__.py
+++ spacy_conll/__init__.py
@@ -5,43 +5,90 @@ from spacy_conll.SpacyConllParser import Spacy2ConllParser
 
 
 class ConllFormatter:
+    """ Pipeline component for spaCy that adds CoNLL-U properties to a Doc and
+        its sentences. A string representation, representation including a
+        header, and the CoNLL-U format in tuples, are added as custom attributes."""
     name = 'conll_formatter'
 
-    def __init__(self, nlp, include_headers=False):
-        self.include_headers = include_headers
+    def __init__(self,
+                 nlp,
+                 *,
+                 conll_str_attr='conll_str',
+                 conll_str_headers_attr='conll_str_headers',
+                 conll_attr='conll'
+                 ):
+        """ ConllFormatter constructor. The extension that are set can be changed with
+            *_attr arguments.
+
+        :param nlp: an initialized spaCy nlp object
+        :param conll_str_attr: an optional string to use as the extension name for conll_str
+        :param conll_str_headers_attr: an optional string to use as the extension name for conll_str_headers
+        :param conll_attr: an optional string to use as the extension name for conll
+        """
         # To get the morphological info, we need a tag map
-        self.tagmap = nlp.Defaults.tag_map
+        self._tagmap = nlp.Defaults.tag_map
+
+        # Set custom attribute names
+        self._attrs = {
+            'conll_str': conll_str_attr,
+            'conll_str_headers': conll_str_headers_attr,
+            'conll': conll_attr
+        }
         self._set_extensions()
 
     def __call__(self, doc):
+        """ Runs the pipeline component, adding the extensions to ._..
+            Adds a string representation, string representation containing a header,
+            and a tuple representation of the CoNLL format to the given Doc and its
+            sentences.
+
+        :param doc: the input Doc
+        :return: the modified Doc containing the newly added extensions
+        """
         # We need to hook the extensions again when using
         # multiprocessing in Windows
         # see: https://github.com/explosion/spaCy/issues/4903
         self._set_extensions()
 
-        sent_conlls = []
+        conll_strs = []
+        conll_strs_w_headers = []
+        conlls = []
         for sent_idx, sent in enumerate(doc.sents, 1):
-            sent_conll = self._get_sent_conll(sent, sent_idx)
-            sent_conlls.append(sent_conll)
-            sent._.set('conll_format', sent_conll)
+            conll_str, conll_str_w_headers, conll = self._get_span_conll(sent, sent_idx)
+            conll_strs.append(conll_str)
+            conll_strs_w_headers.append(conll_str_w_headers)
+            conlls.append(conll)
 
-        doc._.set('conll_format', '\n'.join(sent_conlls))
+            sent._.set(self._attrs['conll_str'], conll_str)
+            sent._.set(self._attrs['conll_str_headers'], conll_str_w_headers)
+            sent._.set(self._attrs['conll'], conll)
+
+        doc._.set(self._attrs['conll_str'], '\n'.join(conll_strs))
+        doc._.set(self._attrs['conll_str_headers'], '\n'.join(conll_strs_w_headers))
+        doc._.set(self._attrs['conll'], conlls)
 
         return doc
 
-    def _get_sent_conll(self, span, span_idx=1):
-        parsed_sent = ''
-        if self.include_headers:
-            parsed_sent += f"# sent_id = {span_idx}\n"
-            parsed_sent += f"# text = {span.text}\n"
+    def _get_span_conll(self, span, span_idx=1):
+        """ Converts a span's properties into CoNLL-U format.
 
+        :param span: a spaCy Span
+        :param span_idx: optional index, corresponding to the n-th sentence
+                         in the parent Doc
+        :return: a string representation, string representation containing a header,
+                 and a tuple representation of the CoNLL format of 'span'
+        """
+        conll_str_w_headers = f"# sent_id = {span_idx}\n# text = {span.text}\n"
+
+        conll_str = ''
+        conll = []
         for word_idx, word in enumerate(span, 1):
             if word.dep_.lower().strip() == 'root':
                 head_idx = 0
             else:
                 head_idx = word.head.i + 1 - span[0].i
 
-            line_tuple = (
+            token_conll = (
                 word_idx,
                 word.text,
                 word.lemma_,
@@ -53,28 +100,45 @@ class ConllFormatter:
                 '_',
                 '_'
             )
-            parsed_sent += '\t'.join(map(str, line_tuple)) + '\n'
+            conll.append(token_conll)
+            conll_str += '\t'.join(map(str, token_conll)) + '\n'
 
-        return parsed_sent
+        conll_str_w_headers += conll_str
+
+        return conll_str, conll_str_w_headers, conll
 
     def _get_morphology(self, tag):
-        if not self.tagmap or tag not in self.tagmap:
+        """ Expands a tag into its morphological features by using a tagmap.
+
+        :param tag: the tag to expand
+        :return: a string entailing the tag's morphological features
+        """
+        if not self._tagmap or tag not in self._tagmap:
             return '_'
         else:
-            feats = [f"{prop}={val}" for prop, val in self.tagmap[tag].items() if not self._is_number(prop)]
+            feats = [f"{prop}={val}" for prop, val in self._tagmap[tag].items() if not self._is_number(prop)]
             if feats:
                 return '|'.join(feats)
             else:
                 return '_'
 
     def _set_extensions(self):
-        if not Span.has_extension('conll_format'):
-            Span.set_extension('conll_format', getter=self._get_sent_conll)
-        if not Doc.has_extension('conll_format'):
-            Doc.set_extension('conll_format', default=None)
+        """ Sets the default extensions if they do not exist yet. """
+        for obj in Span, Doc:
+            if not obj.has_extension(self._attrs['conll_str']):
+                obj.set_extension(self._attrs['conll_str'], default=None)
+            if not obj.has_extension(self._attrs['conll_str_headers']):
+                obj.set_extension(self._attrs['conll_str_headers'], default=None)
+            if not obj.has_extension(self._attrs['conll']):
+                obj.set_extension(self._attrs['conll'], default=None)
+
 
     @staticmethod
     def _is_number(s):
+        """ Checks whether a string is actually a number.
+        :param s: string to test
+        :return: whether or not 's' is a number
+        """
         try:
             float(s)
             return True
