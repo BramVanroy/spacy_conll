a18e1ec5adf49291e750c70bde753716721512b4
Bram Vanroy <Bram.Vanroy@UGent.be>
2021-06-28 11:40:51 +0200

    catch errors when mp goes wrong

--- spacy_conll/parser.py
+++ spacy_conll/parser.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 from locale import getpreferredencoding
 from os import PathLike
 from pathlib import Path
+from pickle import PickleError
 from typing import Union
 
 from spacy import Errors, Language
@@ -65,36 +66,40 @@ class ConllParser:
                        include_headers: bool = False,
                        no_force_counting: bool = False
                        ):
-        # if n_process > 1:
-        #     if self.parser != "spacy":
-        #         raise ValueError("'n_process' > 1 is not supported with 'spacy-udpipe' and 'spacy-stanza'. Set"
-        #                          " 'n_process' to 1 or choose 'spacy' as a parser")
-        #     elif not self.nlp.get_pipe("conll_formatter").disable_pandas:
-        #         raise ValueError("Due to pandas serialisation issues, 'n_process' > 1 is not supported when"
-        #                          " 'disable_pandas' is False in the ConllFormatter. Set 'n_process' to 1 or"
-        #                          " initialise the ConllFormatter with 'disable_pandas=True'")
+        if n_process > 1 and not self.nlp.get_pipe("conll_formatter").disable_pandas:
+            raise ValueError("Due to pandas serialisation issues, 'n_process' > 1 is not supported when"
+                             " 'disable_pandas' is False in the ConllFormatter. Set 'n_process' to 1 or"
+                             " initialise the ConllFormatter with 'disable_pandas=True'")
 
         lines = Path(input_file).resolve().read_text(encoding=input_encoding).splitlines()
         lines = self.prepare_data(lines)
 
         conll_idx = 0
         output = ""
-        for doc_idx, doc in enumerate(self.nlp.pipe(lines, n_process=n_process)):
-            for sent in doc.sents:
-                conll_idx += 1
+        try:
+            for doc_idx, doc in enumerate(self.nlp.pipe(lines, n_process=n_process)):
+                for sent in doc.sents:
+                    conll_idx += 1
 
-                sent_as_conll = sent._.conll_str
-                if include_headers and not no_force_counting:
-                    # nlp.pipe returns different docs, meaning that the generated sentence indices
-                    # by ConllFormatter are not consecutive (they reset for each new doc)
-                    # We can do a regex replace to fix that, though.
-                    sent_as_conll = re.sub(SENT_ID_RE, str(conll_idx), sent_as_conll, 1)
+                    sent_as_conll = sent._.conll_str
+                    if include_headers and not no_force_counting:
+                        # nlp.pipe returns different docs, meaning that the generated sentence indices
+                        # by ConllFormatter are not consecutive (they reset for each new doc)
+                        # We can do a regex replace to fix that, though.
+                        sent_as_conll = re.sub(SENT_ID_RE, str(conll_idx), sent_as_conll, 1)
 
-                # Newline madness dealing with writing to file and printing to stdout at the same time:
-                # Prepend additional newline for all except the very first string.
-                if not (doc_idx == 0 and sent.start == 0):
-                    sent_as_conll = "\n" + sent_as_conll
+                    # Newline madness dealing with writing to file and printing to stdout at the same time:
+                    # Prepend additional newline for all except the very first string.
+                    if not (doc_idx == 0 and sent.start == 0):
+                        sent_as_conll = "\n" + sent_as_conll
 
-                output += sent_as_conll
+                    output += sent_as_conll
+        except (EOFError, TypeError, PickleError) as exc:
+            if n_process > 1:
+                raise ValueError("It seems that something went wrong when processing with 'n_process' > 1. This is not"
+                                 " supported on all platforms/all parsers. Please try again with the default value"
+                                 " 'n_process' = 1.") from exc
+            else:
+                raise exc
 
         return output
