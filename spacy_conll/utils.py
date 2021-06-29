ecbcd222b4249a4d64ef18a7df31f6b014ec8efb
Bram Vanroy <Bram.Vanroy@UGent.be>
2021-06-29 12:04:58 +0200

    remove default spacy parser

--- spacy_conll/utils.py
+++ spacy_conll/utils.py
@@ -1,9 +1,10 @@
-from typing import Dict, Optional, List, Union
+from typing import Dict, List, Optional, Union
 
 import spacy
 from spacy.language import Language
 from spacy.tokens import Doc
 from spacy.vocab import Vocab
+
 
 try:
     import pandas as pd
@@ -12,10 +13,24 @@ try:
 except ImportError:
     PD_AVAILABLE = False
 
+try:
+    import spacy_stanza
+
+    STANZA_AVAILABLE = True
+except ImportError:
+    STANZA_AVAILABLE = False
+
+try:
+    import spacy_udpipe
+
+    UDPIPE_AVAILABLE = True
+except ImportError:
+    UDPIPE_AVAILABLE = False
+
 
 def init_parser(
     model_or_lang: str,
-    parser: str = "spacy",
+    parser: str,
     *,
     is_tokenized: bool = False,
     disable_sbd: bool = False,
@@ -23,17 +38,19 @@ def init_parser(
     **kwargs,
 ) -> Language:
     """Initialise a spacy-wrapped parser given a language or model and some options.
-    :param model_or_lang: language model to use (must be installed). Defaults to an English model
+    :param model_or_lang: language model to use (must be installed for spaCy but will be automatically downloaded for
+           stanza and UDPipe)
     :param parser: which parser to use. Parsers other than 'spacy' need to be installed separately. Valid options are
            'spacy', 'stanza', 'udpipe'. Note that the spacy-* wrappers of those libraries need to be
-           installed, e.g. spacy-stanza. Defaults to 'spacy'
-    :param is_tokenized: indicates whether your text has already been tokenized (space-seperated). For stanza,
-           this will also cause sentence segmentation *only* to be done by splitting on new lines.
-           See the documentation: https://stanfordnlp.github.io/stanza/tokenize.html
+           installed, e.g. spacy-stanza
+    :param is_tokenized: indicates whether your text has already been tokenized (space-seperated). When using 'spacy',
+           this option also disabled sentence segmentation completely. For stanza, sentence segmentation will *only*
+           to be done by splitting on new lines. See the documentation:
+           https://stanfordnlp.github.io/stanza/tokenize.html. This optioon does not affect UDPipe.
     :param disable_sbd: disables spaCy automatic sentence boundary detection (only works for spaCy)
-    :param parser_opts: will be passed to the core pipeline. For spacy and udpipe, it will be passed to their
-           `.load()` initialisations, for stanza `pipeline_opts` is passed to to their `.Pipeline()`
-           initialisations
+    :param parser_opts: will be passed to the core pipeline. For spacy, it will be passed to its
+           `.load()` initialisations, for stanza `pipeline_opts` is passed to its `.load_pipeline()`
+           initialisations. UDPipe does not have any keyword arguments
     :param kwargs: options to be passed to the ConllFormatter initialisation
     :return: an initialised Language object; the parser
     """
@@ -41,20 +58,23 @@ def init_parser(
 
     if parser == "spacy":
         exclude = ["senter", "sentencizer"] if disable_sbd or is_tokenized else []
-        nlp = spacy.load(model_or_lang,  exclude=exclude, **parser_opts)
+        nlp = spacy.load(model_or_lang, exclude=exclude, **parser_opts)
         if is_tokenized:
             nlp.tokenizer = SpacyPretokenizedTokenizer(nlp.vocab)
         if disable_sbd or is_tokenized:
             nlp.add_pipe("disable_sbd", before="parser")
     elif parser == "stanza":
-        import stanza
         import spacy_stanza
+        import stanza
+
         verbose = parser_opts.get("verbose", False)
         stanza.download(model_or_lang, verbose=verbose)
-        nlp = spacy_stanza.load_pipeline(model_or_lang, verbose=verbose, tokenize_pretokenized=is_tokenized,
-                                         **parser_opts)
+        nlp = spacy_stanza.load_pipeline(
+            model_or_lang, verbose=verbose, tokenize_pretokenized=is_tokenized, **parser_opts
+        )
     elif parser == "udpipe":
         import spacy_udpipe
+
         spacy_udpipe.download(model_or_lang)
         nlp = spacy_udpipe.load(model_or_lang)
     else:
@@ -91,7 +111,9 @@ class SpacyPretokenizedTokenizer:
 
 @Language.factory("disable_sbd")
 class SpacyDisableSentenceSegmentation:
-    """Disables spaCy's sentence boundary detection."""
+    """Disables spaCy's sentence boundary detection. In addition, senter and sentencizer components
+       need to be disabled as well."""
+
     def __init__(self, nlp: Language, name: str):
         self.nlp = nlp
         self.name = name
