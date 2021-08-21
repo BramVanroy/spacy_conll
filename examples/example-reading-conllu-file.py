229b432f9d7beeeaf2906ca80a40bf543a6745cc
shaked571 <shakedgreenfeld@gmail.com>
2021-08-21 18:21:49 +0300

    add example usage

--- examples/example-reading-conllu-file.py
+++ examples/example-reading-conllu-file.py
@@ -1,5 +1,6 @@
 from spacy_conll import init_parser
 from spacy_conll.parser import ConllParser
+import os
 
 """Example showing how to use spacy_conll to easily read a ConLL doc"""
 
@@ -7,12 +8,11 @@ from spacy_conll.parser import ConllParser
 def main():
     # Initialise English parser, already including the ConllFormatter as a pipeline component
     nlp = init_parser("en_core_web_sm", "spacy")
-    # Parse a given string
     parser = ConllParser(nlp)
-    docs = parser.parse_conll_as_spacy(
-        r"C:\Dev\spacy_conll\tests\en_ewt-ud-dev.conllu-sample.txt",
-        "utf-8",
-        merge_subtoken=False)
+    # Path to a conllu file
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "tests", "data_file",
+                        "en_ewt-ud-dev.conllu-sample.txt")
+    docs = parser.parse_conll_as_spacy(path, "utf-8", merge_subtoken=False)
     for d in docs:
         print(d)
 
