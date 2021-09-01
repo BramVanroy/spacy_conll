27e6e953cb9e607bbfc5041accc48ed72188c0ff
Bram Vanroy <Bram.Vanroy@UGent.be>
2021-09-01 13:26:07 +0200

    multiple improvements
    - do not support MWT or empty nodes
    - raise errors where appropriate
    - include morph
    - use Doc() constructor more widely
    - instead of generator, return one Doc per CoNLL file
    - set custom extension for properties that CoNLL supports but spaCy does not (dep graph, misc)
    - set custom properties for the metadata

--- spacy_conll/parser.py
+++ spacy_conll/parser.py
@@ -4,11 +4,12 @@ from dataclasses import dataclass, field
 from locale import getpreferredencoding
 from os import PathLike
 from pathlib import Path
-from typing import List, Union, Generator
+from typing import Dict, List, Union
 
 from spacy import Errors, Language
-from spacy.tokens import Doc, Token
-
+from spacy.tokens import Doc, Span, Token
+from spacy.training.converters.conllu_to_docs import get_entities
+from spacy.training.iob_utils import spans_from_biluo_tags
 from spacy_conll.utils import STANZA_AVAILABLE, UDPIPE_AVAILABLE
 
 
@@ -17,7 +18,6 @@ if STANZA_AVAILABLE:
 
 if UDPIPE_AVAILABLE:
     from spacy_udpipe import UDPipeTokenizer
-
 
 SENT_ID_RE = re.compile(r"(?<=# sent_id = )(\d+)")
 
@@ -80,7 +80,6 @@ class ConllParser:
         :param input_encoding: encoding of 'input_file'
         :param kwargs: keyword arguments that will be passed to `parse_text_as_conll`
         """
-
         text = Path(input_file).resolve().read_text(encoding=input_encoding)
 
         return self.parse_text_as_conll(text, **kwargs)
@@ -152,175 +151,135 @@ class ConllParser:
 
         return output
 
-
-    def parse_conll_as_spacy(self, input_file: Union[PathLike, Path, str],
-                             input_encoding: str = getpreferredencoding(),
-                             merge_subtoken: bool = False,
-                             ) -> Generator[Doc]:
-        """
-        Parses a given file in a conll format and return it as a list of spacy documents
+    def parse_conll_file_as_spacy(
+        self,
+        input_file: Union[PathLike, Path, str],
+        input_encoding: str = getpreferredencoding(),
+        ner_tag_pattern: str = "^((?:name|NE)=)?([BILU])-([A-Z_]+)|O$",
+        ner_map: Dict[str, str] = None,
+    ) -> Doc:
+        """Parses a given CoNLL-U file into a spaCy doc. Parsed sentence section must be separated by a new line.
+        See :py:meth:`ConllParser.parse_conll_text_as_spacy`.
         :param input_file: path to the input file to process
         :param input_encoding: encoding of 'input_file'
-        :param merge_subtoken:  whether or not merge subtokens into a single one.
-        :return: a generator of spacy docs
+        :param ner_tag_pattern: Regex pattern for entity tag in the MISC field
+        :param ner_map: Map old NER tag names to new ones, '' maps to O
+        :return: a spacy Doc containing all the tokens and sentences from the CoNLL file including the custom CoNLL extensions
         """
         text = Path(input_file).resolve().read_text(encoding=input_encoding)
+        return self.parse_conll_text_as_spacy(text, ner_tag_pattern=ner_tag_pattern, ner_map=ner_map)
 
-        return self.read_conllx(text,merge_subtoken=merge_subtoken)
+    def parse_conll_text_as_spacy(
+        self,
+        text: str,
+        ner_tag_pattern: str = "^((?:name|NE)=)?([BILU])-([A-Z_]+)|O$",
+        ner_map: Dict[str, str] = None,
+    ) -> Doc:
+        """Parses a given CoNLL-U string into a spaCy doc. Parsed sentence section must be separated by a new line (\n\n).
+        Note that we do our best to retain as much information as possible but that not all CoNLL-U fields are
+        supported in spaCy. We add a Token._.conll_misc_field extension to save CoNLL-U MISC field, and a
+        Token._.conll_deps_graphs_field extension to save CoNLL-U DEPS field. The metadata (lines starting with #)
+        is saved in Span._.conll_metadata of sentence Spans.
 
-    def read_conllx(
-            self,
-            input_data: List[str],
-            merge_subtoken: bool,
+        This method has been adapted from the work by spaCy.
+        See: https://github.com/explosion/spaCy/blob/a1c5b694be117ac92e21f9860309821ad6da06f7/spacy/cli/converters/conllu2json.py#L179
 
-    )-> Generator[Doc]:
+        Multi-word tokens and empty nodes are not supported.
+
+        :param text: CoNLL-U formatted text
+        :param ner_tag_pattern: Regex pattern for entity tag in the MISC field
+        :param ner_map: Map old NER tag names to new ones, '' maps to O
+        :return: a spacy Doc containing all the tokens and sentences from the CoNLL file including
+         the custom CoNLL extensions
         """
-         Yield examples, one for each sentence
-        :param input_file: path to the input file to process
-        :param merge_subtoken:  whether or not merge subtokens into a single one.
-        :return:  a generator of spacy docs
-        """
-        for sent in input_data.strip().split("\n\n"):
-            lines = sent.strip().split("\n")
-            if lines:
-                while lines[0].startswith("#"):
-                    lines.pop(0)
-                example = self._example_from_conllu_sentence(
-                    lines,
-                    merge_subtoken=merge_subtoken,
-                )
-                yield example
+        if not Token.has_extension("conll_misc_field"):
+            Token.set_extension("conll_misc_field", default="_")
+        if not Token.has_extension("conll_deps_graphs_field"):
+            Token.set_extension("conll_deps_graphs_field", default="_")
+        if not Span.has_extension("conll_metadata"):
+            Span.set_extension("conll_metadata", default=None)
 
+        docs = []
+        for chunk in text.split("\n\n"):
+            lines = [l for l in chunk.splitlines() if l and not l.startswith("#")]
+            words, spaces, tags, poses, morphs, lemmas, miscs = [], [], [], [], [], [], []
+            heads, deps, deps_graphs = [], [], []
+            for i in range(len(lines)):
+                line = lines[i]
+                parts = line.split("\t")
 
-    def _example_from_conllu_sentence(
-            self,
-            lines: List[str],
-            merge_subtoken: bool =False,
-    )-> Doc:
-        """
-        Create an Example from the lines for one CoNLL-U sentence, merging
-        subtokens and appending morphology to tags if required.
-        :param lines:  The non-comment lines for a CoNLL-U sentence
-        :param merge_subtoken:  whether or not merge subtokens into a single one.
-        :return: a spacy doc rpresenting a conll sentence
-        """
-        # create a Doc with each subtoken as its own token
-        # if merging subtokens, each subtoken orth is the merged subtoken form
-        if not Token.has_extension("merged_orth"):
-            Token.set_extension("merged_orth", default="")
-        if not Token.has_extension("merged_lemma"):
-            Token.set_extension("merged_lemma", default="")
-        if not Token.has_extension("merged_morph"):
-            Token.set_extension("merged_morph", default="")
-        if not Token.has_extension("merged_spaceafter"):
-            Token.set_extension("merged_spaceafter", default="")
-        words, spaces, tags, poses, morphs, lemmas = [], [], [], [], [], []
-        heads, deps = [], []
-        subtok_word = ""
-        in_subtok = False
-        for i in range(len(lines)):
-            line = lines[i]
-            parts = line.split("\t")
-            id_, word, lemma, pos, tag, morph, head, dep, _1, misc = parts
-            if "." in id_:
-                continue
-            if "-" in id_:
-                in_subtok = True
-            if "-" in id_:
-                in_subtok = True
-                subtok_word = word
-                subtok_start, subtok_end = id_.split("-")
-                subtok_spaceafter = "SpaceAfter=No" not in misc
-                continue
-            if merge_subtoken and in_subtok:
-                words.append(subtok_word)
-            else:
-                words.append(word)
-            if in_subtok:
-                if id_ == subtok_end:
-                    spaces.append(subtok_spaceafter)
-                else:
-                    spaces.append(False)
-            elif "SpaceAfter=No" in misc:
-                spaces.append(False)
-            else:
-                spaces.append(True)
-            if in_subtok and id_ == subtok_end:
-                subtok_word = ""
-                in_subtok = False
-            id_ = int(id_) - 1
-            head = (int(head) - 1) if head not in ("0", "_") else id_
-            tag = pos if tag == "_" else tag
-            morph = morph if morph != "_" else ""
-            dep = "ROOT" if dep == "root" else dep
-            lemmas.append(lemma)
-            poses.append(pos)
-            tags.append(tag)
-            morphs.append(morph)
-            heads.append(head)
-            deps.append(dep)
-
-        doc: Doc = Doc(self.nlp.vocab, words=words, spaces=spaces)
-        for i in range(len(doc)):
-            doc[i].tag_ = tags[i]
-            doc[i].pos_ = poses[i]
-            doc[i].dep_ = deps[i]
-            doc[i].lemma_ = lemmas[i]
-            doc[i].head = doc[heads[i]]
-            doc[i]._.merged_orth = words[i]
-            doc[i]._.merged_morph = morphs[i]
-            doc[i]._.merged_lemma = lemmas[i]
-            doc[i]._.merged_spaceafter = spaces[i]
-        if merge_subtoken:
-            doc = self._merge_conllu_subtokens(lines, doc)
-        return doc
-
-    def _merge_conllu_subtokens(self, lines: List[str], doc: Doc)-> Doc:
-        """
-        merging conllu subtokens to a single one
-        :param lines: the lines in conllu file
-        :param doc: the spacy document with unmerged tokens
-        :return: the spacy document with merged tokens
-        """
-        # identify and process all subtoken spans to prepare attrs for merging
-        subtok_spans = []
-        for line in lines:
-            parts = line.split("\t")
-            id_, word, lemma, pos, tag, morph, head, dep, _1, misc = parts
-            if "-" in id_:
-                subtok_start, subtok_end = id_.split("-")
-                subtok_span = doc[int(subtok_start) - 1 : int(subtok_end)]
-                subtok_spans.append(subtok_span)
-                # create merged tag, morph, and lemma values
-                tags = []
-                morphs = {}
-                lemmas = []
-                for token in subtok_span:
-                    tags.append(token.tag_)
-                    lemmas.append(token.lemma_)
-                    if token._.merged_morph:
-                        for feature in token._.merged_morph.split("|"):
-                            field, values = feature.split("=", 1)
-                            if field not in morphs:
-                                morphs[field] = set()
-                            for value in values.split(","):
-                                morphs[field].add(value)
-                # create merged features for each morph field
-                for field, values in morphs.items():
-                    morphs[field] = field + "=" + ",".join(sorted(values))
-                # set the same attrs on all subtok tokens so that whatever head the
-                # retokenizer chooses, the final attrs are available on that token
-                for token in subtok_span:
-                    token._.merged_orth = token.orth_
-                    token._.merged_lemma = " ".join(lemmas)
-                    token.tag_ = "_".join(tags)
-                    token._.merged_morph = "|".join(sorted(morphs.values()))
-                    token._.merged_spaceafter = (
-                        True if subtok_span[-1].whitespace_ else False
+                if any(not p for p in parts):
+                    raise ValueError(
+                        "According to the CoNLL-U Format, fields cannot be empty. See"
+                        " https://universaldependencies.org/format.html"
                     )
 
-        with doc.retokenize() as retokenizer:
-            for span in subtok_spans:
-                retokenizer.merge(span)
+                id_, word, lemma, pos, tag, morph, head, dep, deps_graph, misc = parts
 
-        return doc
+                if any(" " in f for f in (id_, pos, tag, morph, head, dep, deps_graph)):
+                    raise ValueError(
+                        "According to the CoNLL-U Format, only FORM, LEMMA, and MISC fields can contain"
+                        " spaces. See https://universaldependencies.org/format.html"
+                    )
+
+                if "." in id_ or "-" in id_:
+                    raise NotImplementedError(
+                        "Multi-word tokens and empty nodes are not supported in spacy_conll"
+                    )
+
+                words.append(word)
+
+                if "SpaceAfter=No" in misc:
+                    spaces.append(False)
+                else:
+                    spaces.append(True)
+
+                id_ = int(id_) - 1
+                lemmas.append(lemma)
+                poses.append(pos)
+                tags.append(pos if tag == "_" else tag)
+                morphs.append(morph if morph != "_" else "")
+                heads.append((int(head) - 1) if head not in ("0", "_") else id_)
+                deps.append("ROOT" if dep == "root" else dep)
+                deps_graphs.append(deps_graph)
+                miscs.append(misc)
+
+            doc = Doc(
+                self.nlp.vocab,
+                words=words,
+                spaces=spaces,
+                tags=tags,
+                pos=poses,
+                morphs=morphs,
+                lemmas=lemmas,
+                heads=heads,
+                deps=deps,
+            )
+
+            # Set custom Token extensions
+            for i in range(len(doc)):
+                doc[i]._.conll_misc_field = miscs[i]
+                doc[i]._.conll_deps_graphs_field = deps_graphs[i]
+
+            ents = get_entities(lines, ner_tag_pattern, ner_map)
+            doc.ents = spans_from_biluo_tags(doc, ents)
+
+            # The deprel relations ensure that this CoNLL chunk is one sentence
+            # Deprel cannot therefore not be empty or each word is considered a separate sentence
+            if len(list(doc.sents)) != 1:
+                raise ValueError(
+                    "Your data is in an unexpected format. Make sure that it follows the CoNLL-U format"
+                    " requirements. See https://universaldependencies.org/format.html. Particularly make"
+                    " sure that the DEPREL field is filled in."
+                )
+
+            # Save the metadata in a custom sentence Span attribute so that the formatter can use it
+            metadata = "\n".join([l for l in chunk.splitlines() if l.startswith("#")])
+            # We really only expect one sentence
+            for sent in doc.sents:
+                sent._.conll_metadata = f"{metadata}\n" if metadata else ""
+
+            docs.append(doc)
+
+        # Add CoNLL custom extensions
+        return self.nlp.get_pipe("conll_formatter")(Doc.from_docs(docs))
