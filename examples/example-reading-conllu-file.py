29fb84645f1cea683b903fb3838061e8550526e9
Bram Vanroy <Bram.Vanroy@UGent.be>
2021-09-01 13:26:47 +0200

    Update example-reading-conllu-file.py

--- examples/example-reading-conllu-file.py
+++ examples/example-reading-conllu-file.py
@@ -1,20 +1,24 @@
+from pathlib import Path
+
 from spacy_conll import init_parser
 from spacy_conll.parser import ConllParser
-import os
 
-"""Example showing how to use spacy_conll to easily read a ConLL doc"""
+
+"""Example showing how to use spacy_conll to read a ConLL file as a spaCy doc."""
 
 
 def main():
-    # Initialise English parser, already including the ConllFormatter as a pipeline component
-    nlp = init_parser("en_core_web_sm", "spacy")
+    # Initialise English spaCy parser, already including the ConllFormatter as a pipeline component
+    nlp = init_parser("en_core_web_sm", "spacy", include_headers=True)
     parser = ConllParser(nlp)
-    # Path to a conllu file
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "tests",
-                        "en_ewt-ud-dev.conllu-sample.txt")
-    docs = parser.parse_conll_as_spacy(path, "utf-8", merge_subtoken=False)
-    for d in docs:
-        print(d)
+    # Path to a CoNLL-U test file
+    path = Path(__file__).parent.parent / "tests" / "en_ewt-ud-dev.conllu-sample.txt"
+    doc = parser.parse_conll_file_as_spacy(path, "utf-8")
+    for sent_id, sent in enumerate(doc.sents, 1):
+        print(sent._.conll_str)
+        for word in sent:
+            print(word, word.dep_)
+        print()
 
 
 if __name__ == "__main__":
