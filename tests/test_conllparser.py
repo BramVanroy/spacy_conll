d1354399d2e7ba85125a654f7727b3040f866191
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2024-07-02 10:22:21 +0200

    make style

--- tests/test_conllparser.py
+++ tests/test_conllparser.py
@@ -20,6 +20,6 @@ def test_pretokenized_conllparser(pretokenized_conllparser_conllstr):
 def test_conllparser_n_process(conllparser):
     # error due to pandas
     with pytest.raises(OSError) as e_info:
-        conllparser.parse_file_as_conll(Path(__file__).parent.joinpath("test.txt"),
-                                        input_encoding="utf-8",
-                                        n_process=2)
+        conllparser.parse_file_as_conll(
+            Path(__file__).parent.joinpath("test.txt"), input_encoding="utf-8", n_process=2
+        )
